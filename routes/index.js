7821c8e745826df925e4e61040033e731eb74db7
Gabriel <gabriel.dcomtois@gmail.com>
2017-12-04 22:36:15 -0500

    almost done

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,89 @@ var express = require('express');
 var http = require('http');
 var winston = require('winston');
 var csv = require('csv');
+var raml2html = require('raml2html');
 var pools = require('../lib/fetcher.js');
+var db = require('../lib/db');
+var mongodb = require('mongodb');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express'
+router.get('/', function(req, res) {
+  
+  db.getConnection((err, db)=>{
+    db.collection('installations', (err, collection)=>{
+      if(err){
+        winston.error(err);
+        res.sendStatus(500);
+      }else{
+        var inst = collection.distinct("nom").then((result)=>{
+          res.render('index',{title:'INF4375 - Installations de la Ville de Montreal',installations:result});
+        });
+        
+
+       
+      }
+    });
+  });
+
+
+
+  
+});
+
+/* GET doc page. */
+router.get('/doc', function(req, res) {
+  var config = raml2html.getConfigForTheme();
+  var onError = (err)=>{
+    winston.error(err);
+    res.sendStatus(500);
+  };
+  var onSuccess = (html)=>{
+    res.send(html);
+  };
+  raml2html.render("routes/doc/index.raml", config).then(onSuccess, onError);
+});
+
+//get installation
+//param: arrrondissement
+router.get('/installations',(req,res)=>{
+ db.getConnection((err, db)=>{
+    db.collection('installations', (err, collection)=>{
+      if(err){
+        winston.error(err);
+        res.sendStatus(500);
+      }else{
+        if(req.query.arrondissement!=null){
+          console.log(req.query.arrondissement);
+          collection.find({$or:[{"arrondissement":req.query.arrondissement},{"arrondissement.0.nom_arr.0":req.query.arrondissement}]}).toArray((err,result)=>{
+            if(err){
+              winston.error(err);
+              res.sendStatus(500);
+            }else{
+              res.json(result);
+            }
+          });
+        }else if(req.query.condition!=null){
+          collection.find({"condition.0":req.query.condition}).toArray((err,result)=>{
+            if(err){
+              winston.error(err);
+              res.sendStatus(500);
+            }else{
+              res.json(result.sort((a, b)=> {return a.nom - b.nom;}));
+            }
+          });
+        }else if(req.query.nom!=null){
+          collection.find({"nom":req.query.nom}).toArray((err,result)=>{
+            if(err){
+              winston.error(err);
+              res.sendStatus(500);
+            }else{
+              res.json(result.sort((a, b)=> {return a.nom - b.nom;}));
+            }
+          });
+        }
+      }
+    });
   });
 });
 
