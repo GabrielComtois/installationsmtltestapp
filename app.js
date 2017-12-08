0d3f0a24d6d22f7f810f8e8b79f122cf2d770d9c
Gabriel <gabriel.dcomtois@gmail.com>
2017-12-08 11:27:13 -0500

    added http

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
 var app = express();
-
+app.use('/', index);
+app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,9 +19,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
